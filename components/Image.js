fe45b14c2adbe721b9c9ef3b24e3c041d1902563
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2023-07-26 13:00:38 +0100

    Attempt to fix images on deployment

--- components/Image.js
+++ components/Image.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line jsx-a11y/alt-text, prettier/prettier, @next/next/no-img-element
 
-const Image = ({ src, alt, ...rest }) => <img src={src} alt={alt} {...rest} />
+const Image = ({ src, alt, ...rest }) => (
+  <img src={`${process.env.BASE_PATH || ''}${src}`} alt={alt} {...rest} />
+)
 
 export default Image
