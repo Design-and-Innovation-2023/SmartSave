98ea7478361f735e44d1b40d218ee0e673b86d16
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2023-07-26 13:11:08 +0100

    2nd attempt at fixing image in deployment

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,9 @@
+import getConfig from 'next/config'
 // eslint-disable-next-line jsx-a11y/alt-text, prettier/prettier, @next/next/no-img-element
 
-const Image = ({ src, alt, ...rest }) => (
-  <img src={`${process.env.BASE_PATH || ''}${src}`} alt={alt} {...rest} />
-)
+const Image = ({ src, alt, ...rest }) => {
+  const { publicRuntimeConfig } = getConfig()
+  return <img src={`${publicRuntimeConfig.basePath || ''}${src}`} alt={alt} {...rest} />
+}
 
 export default Image
