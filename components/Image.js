2b0d7b42e0d3ec98e40cdc21758666c49c011031
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2023-07-26 12:34:46 +0100

    Disable some linting

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ src, alt, ...rest }) => (
-    <img src={src} alt={alt} {...rest} />
-  );
+// eslint-disable-next-line jsx-a11y/alt-text, prettier/prettier, @next/next/no-img-element
+
+const Image = ({ src, alt, ...rest }) => <img src={src} alt={alt} {...rest} />
 
 export default Image
