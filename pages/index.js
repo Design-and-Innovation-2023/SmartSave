6dc35eb179e6404f515e0403e4b1c8230823664d
Arcadial <arshadmazlan@outlook.sg>
2023-08-02 06:08:43 +0800

    Finish up landing page

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,60 @@ export default function Home() {
   const introHeaderVariants = {
     hide: {
       opacity: 0,
-      x: -500,
+      x: -150,
     },
     show: {
       opacity: 1,
       x: 0,
       transition: {
-        duration: 2,
+        duration: 1,
       },
     },
   }
-  const cardVariants = {}
+  const cardVariants = {
+    hide: {
+      opacity: 0,
+      y: 100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeOut',
+        duration: 1,
+      },
+    },
+  }
+  const cardVariants1 = {
+    hide: {
+      opacity: 0,
+      y: 100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeOut',
+        duration: 1,
+        delay: 0.2,
+      },
+    },
+  }
+  const cardVariants2 = {
+    hide: {
+      opacity: 0,
+      y: 100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeOut',
+        duration: 1,
+        delay: 0.4,
+      },
+    },
+  }
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
@@ -53,7 +96,13 @@ export default function Home() {
         Our features are <em>designed</em> to help you save effectively, and on any device.
       </p>
       <span className="mt-20 inline-grid grid-cols-3 gap-44">
-        <div className="text-center drop-shadow-md hover:drop-shadow-xl">
+        <motion.div
+          className="text-center drop-shadow-md hover:drop-shadow-xl"
+          initial="hide"
+          whileInView="show"
+          exit="hide"
+          variants={cardVariants}
+        >
           <div className="inline-block rounded bg-neutral-50">
             <Image
               src="/static/images/cost-effective.png"
@@ -64,8 +113,14 @@ export default function Home() {
             />
           </div>
           <p className="mt-2 text-xl font-medium">Cost-effective</p>
-        </div>
-        <div className="text-center drop-shadow-md hover:drop-shadow-xl">
+        </motion.div>
+        <motion.div
+          className="text-center drop-shadow-md hover:drop-shadow-xl"
+          initial="hide"
+          whileInView="show"
+          exit="hide"
+          variants={cardVariants1}
+        >
           <div className="inline-block rounded bg-neutral-50">
             <Image
               src="/static/images/swift.png"
@@ -76,8 +131,14 @@ export default function Home() {
             />
           </div>
           <p className="mt-2 text-xl font-medium">Swift</p>
-        </div>
-        <div className="text-center drop-shadow-md hover:drop-shadow-xl">
+        </motion.div>
+        <motion.div
+          className="text-center drop-shadow-md hover:drop-shadow-xl"
+          initial="hide"
+          whileInView="show"
+          exit="hide"
+          variants={cardVariants2}
+        >
           <div className="inline-block rounded bg-neutral-50">
             <Image
               src="/static/images/multiplatform.png"
@@ -88,9 +149,9 @@ export default function Home() {
             />
           </div>
           <p className="mt-2 text-xl font-medium">Multi-platform</p>
-        </div>
+        </motion.div>
       </span>
-      <div className="mt-32 mb-8">
+      <div className="mt-24 mb-4">
         <div className="flex shrink-0 flex-col items-center">
           <button
             onClick={() => {
