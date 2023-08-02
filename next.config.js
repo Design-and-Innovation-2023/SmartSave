055cf7d9de87a2748ac598f8a136a68b58073694
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2023-08-02 14:31:22 +0100

    Add Figma to CSP frame-src whitelist

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app www.youtube.com;
+  frame-src giscus.app www.youtube.com www.figma.com;
 `
 
 const securityHeaders = [
