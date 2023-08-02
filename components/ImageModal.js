388e075d1e67c23cd61bfe379ddae608438e73e1
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2023-08-02 18:31:56 +0100

    Add ImageModal component

---
+++ components/ImageModal.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import Image from './Image'
+
+function ImageModal({ src }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <Image
+        src={src}
+        alt="Thumbnail"
+        onClick={() => setIsOpen(true)}
+        className="cursor-pointer object-cover"
+      />
+
+      {isOpen && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60"
+          onClick={() => setIsOpen(false)}
+        >
+          <button
+            onClick={(e) => {
+              e.stopPropagation()
+              setIsOpen(false)
+            }}
+            className="absolute top-4 right-4 text-xl text-white font-bold px-2 py-1"
+          >
+            X
+          </button>
+          <img src={src} alt="Full size" className="max-w-full max-h-full" />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default ImageModal
