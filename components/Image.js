914daf69e7fcd7198048df6727e0a2713ebeb321
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2023-08-02 14:53:36 +0100

    Update Image basePath hack

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,15 @@
 // eslint-disable-next-line jsx-a11y/alt-text, prettier/prettier, @next/next/no-img-element
 
-const Image = ({ src, alt, ...rest }) => (
-  <img src={`${process.env.BASE_PATH || ''}${src}`} alt={alt} {...rest} />
-)
+const Image = ({ src, alt, ...rest }) => {
+  let basePath = ''
+
+  try {
+    basePath = process.env.BASE_PATH || ''
+  } catch {
+    basePath = ''
+  }
+
+  return <img src={`${basePath}${src}`} alt={alt} {...rest} />
+}
 
 export default Image
